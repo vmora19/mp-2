eb164cb2c1ab2d5b9393f379ee63f64bd5f68ff8
Valentina Mora <vmora19@bu.edu>
2025-09-28 16:28:45 -0400

    completed additional 3 fields to rick and morty api

---
+++ src/interfaces/Charcters.ts
@@ -0,0 +1,11 @@
+
+export interface Character{
+    id: number;
+    name: string;
+    image: string;
+    species: string;
+    status: string;
+    type: string;
+    gender: string;
+    created: string;
+}
